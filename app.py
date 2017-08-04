4e4a67dcee67ff831f716ad2f642aba2d2704c23
sho@mozilla.com <sho@mozilla.com>
2017-08-04 14:35:25 +0800

    modify app.py
    update button
    
    update button
    
    update button
    
    update button
    
    update button
    
    update button
    
    update button
    
    update button
    
    update button
    
    update button
    
    update button
    
    update
    
    update
    
    update
    
    update
    
    update
    
    update
    
    mes
    
    test
    
    squash

--- app.py
+++ app.py
@@ -7,9 +7,7 @@ from linebot import (
 from linebot.exceptions import (
     InvalidSignatureError
 )
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
-)
+from linebot.models import *
 
 app = Flask(__name__)
 
@@ -33,13 +31,93 @@ def callback():
 
     return 'OK'
 
+@handler.add(PostbackEvent)
+def handle_text_message(event):
+    print event.postback.data
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_text_message(event):
+    profile = line_bot_api.get_profile(event.source.user_id)
+
     text = event.message.text #message from user
 
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=text)) #reply the same message from user
+    if text.lower().strip() == "show-me-button":
+        line_bot_api.reply_message(event.reply_token, TemplateSendMessage(
+            alt_text='Buttons template',
+            template=ButtonsTemplate(
+                thumbnail_image_url='https://www.everrich-group.com/Upload/fe5ac954-dcc5-479e-8cbe-ffa90cf14d41/TC/cover.jpg',
+                title='Menu',
+                text='Please select',
+                actions=[
+                    PostbackTemplateAction(
+                        label='postback',
+                        text='postback text',
+                        data='action=buy&itemid=1'
+                    ),
+                    MessageTemplateAction(
+                        label='message',
+                        text='message text'
+                    ),
+                    URITemplateAction(
+                        label='uri',
+                        uri='http://example.com/'
+                    )
+                ]
+            )
+        ))
+    elif text.lower().strip() == "show-me-slideshow":
+        line_bot_api.reply_message(event.reply_token, TemplateSendMessage(
+            alt_text='Carousel template',
+            template=CarouselTemplate(
+                columns=[
+                    CarouselColumn(
+                        thumbnail_image_url='https://www.everrich-group.com/Upload/fe5ac954-dcc5-479e-8cbe-ffa90cf14d41/TC/cover.jpg',
+                        title='Menu1',
+                        text='Desc1',
+                        actions=[
+                            PostbackTemplateAction(
+                                label='Purchase',
+                                text=profile.display_name + ' thank you for buying this',
+                                data='data=1'
+                            ),
+                            MessageTemplateAction(
+                                label='Detail',
+                                text='This is a great tea'
+                            ),
+                            URITemplateAction(
+                                label='U1',
+                                uri='http://abc.com/1'
+                            )
+                        ]
+                    ),
+                    CarouselColumn(
+                        thumbnail_image_url='https://www.everrich-group.com/Upload/fe5ac954-dcc5-479e-8cbe-ffa90cf14d41/TC/cover.jpg',
+                        title='Menu2',
+                        text='Desc2',
+                        actions=[
+                            PostbackTemplateAction(
+                                label='Purchase',
+                                text=profile.display_name + ' thank you for buying this',
+                                data='data=2'
+                            ),
+                            MessageTemplateAction(
+                                label='Detail',
+                                text='This is the best tea'
+                            ),
+                            URITemplateAction(
+                                label='U2',
+                                uri='http://abc.com/2'
+                            )
+                        ]
+                    )
+                ]
+            )
+        ))
+    else:
+        print text
+        #line_bot_api.reply_message(
+        #    event.reply_token,
+        #    TextSendMessage(text=text)) #reply the same message from user
     
 
 import os
